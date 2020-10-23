daf55b3a86be7835cd54d006e14e203b410d861d
Eduardo Hurtado <leduardohurtado@estudiante.uniajc.edu.co>
2020-10-22 20:46:39 -0500

    Testing

--- src/goCode.go
+++ src/goCode.go
@@ -11,6 +11,12 @@ var (
 	season       int    = 3
 )
 
+const (
+	a3 = iota
+	a4 = iota
+	a5 = iota
+)
+
 func main() {
 
 	// var u int = 44	// Never used error
@@ -34,5 +40,39 @@ func main() {
 	fmt.Println(l)
 	fmt.Println(m)
 
+	// Any var initialized has 0 or false as value
+	var poop int
+	fmt.Println("Default value:", poop)
+
 	// Numeric types
+
+	// Unsigned integer
+	var noSign uint = 12
+	fmt.Println("Unsigned:", noSign)
+
+	// Logical operators
+	fmt.Println("Logical operators:")
+	a := 10 // 1010
+	b := 3  // 0011
+
+	fmt.Println(a & b)  // 0010
+	fmt.Println(a | b)  // 1011
+	fmt.Println(a ^ b)  // 1001
+	fmt.Println(a &^ b) // 0100
+
+	// Bit shift ONLY INTEGER
+	fmt.Println("Bit shift")
+	a = 8               // 2^3
+	fmt.Println(a << 3) // 2^3 * 2^3 = 2^6
+	fmt.Println(a >> 3) // 2^3 / 2^3 = 2^0
+
+	// Type string
+	s := "This is a string"
+	fmt.Println(s)
+	fmt.Printf("%v, %T\n", string(s[2]), s[2])
+
+	// Constants, you cant set your constants at run time ex: const a int = func(1.57)
+	fmt.Println("My constant:")
+	const myConst int = 98
+	fmt.Printf("%v, %T\n", myConst, myConst)
 }
